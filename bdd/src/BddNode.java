686d3dd17dee997465a37bb8caa5f3d6c6938be8
Alexander Moriarty <alexander@dal.ca>
2013-05-31 16:21:53 +0200

    started bdd assginment

---
+++ bdd/src/BddNode.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+
+/**
+ * @author alex
+ *
+ */
+public class BddNode {
+
+    /**
+     * @param args
+     */
+    private BddNode high; 
+    private BddNode low; 
+    int variable;
+
+    BddNode(int newData) { 
+      setHigh(null); 
+      setLow(null); 
+      variable = newData; 
+    }
+
+    public BddNode getHigh() {
+        return high;
+    }
+
+    public void setHigh(BddNode high) {
+        this.high = high;
+    }
+
+    public BddNode getLow() {
+        return low;
+    }
+
+    public void setLow(BddNode low) {
+        this.low = low;
+    } 
+
+}
