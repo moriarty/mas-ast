25edd0b794c12fb70893f128448b45619b2266e0
Alexander Moriarty <alexander@dal.ca>
2013-06-07 16:34:27 +0200

    added some todo comments to empty test file

--- bdd/src/BddTestHardCoded.java
+++ bdd/src/BddTestHardCoded.java
@@ -12,7 +12,9 @@ public class BddTestHardCoded {
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Create a Hard BDD Tree
+        // TODO Create a Hard BDD Tree and test our functions
+        
+        // Create a Hashmap or ArrayList which we will add our Nodes to.
         
         // Create Ending Nodes 
         // Node Zero
@@ -33,6 +35,9 @@ public class BddTestHardCoded {
         // Node F
         
         // Test some functions
+        
+        
+        // Print out the Tree.
 
     }
 
