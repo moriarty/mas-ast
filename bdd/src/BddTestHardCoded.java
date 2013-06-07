846e69850731a73f2d6b4907d4e0306f9fe2e5b6
Alexander Moriarty <alexander@dal.ca>
2013-06-07 16:31:42 +0200

    Added a empty HardCoded Test. Simple Tree to be implemented for our first tests.

---
+++ bdd/src/BddTestHardCoded.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+
+/**
+ * @author alex
+ *
+ */
+public class BddTestHardCoded {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Create a Hard BDD Tree
+        
+        // Create Ending Nodes 
+        // Node Zero
+        
+        // Node One
+        
+        // Create Regular Nodes
+        // Node A (root) 
+        
+        // Node B 
+        
+        // Node C
+        
+        // Node D
+        
+        // Node E
+        
+        // Node F
+        
+        // Test some functions
+
+    }
+
+}
